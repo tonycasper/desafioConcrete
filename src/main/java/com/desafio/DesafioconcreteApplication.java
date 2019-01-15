788a3975095c331cdfd44ac81cc7dbea617daa01
deehgaspar <andregaspar_@hotmail.com>
2019-01-15 21:39:32 -0200

    first commit

---
+++ src/main/java/com/desafio/DesafioconcreteApplication.java
@@ -0,0 +1,14 @@
+package com.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioconcreteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioconcreteApplication.class, args);
+	}
+
+}
+
