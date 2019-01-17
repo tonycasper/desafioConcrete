207adf2cad336027e9bce506d5a50164a8dde908
deehgaspar <andregaspar_@hotmail.com>
2019-01-17 21:24:46 -0200

    arrumando commit inicial com o gradle

--- src/main/java/com/concrete/desafio/DesafioApplication.java
+++ src/main/java/com/concrete/desafio/DesafioApplication.java
@@ -1,13 +1,13 @@
-package com.desafio;
+package com.concrete.desafio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DesafioconcreteApplication {
+public class DesafioApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DesafioconcreteApplication.class, args);
+		SpringApplication.run(DesafioApplication.class, args);
 	}
 
 }
--- src/test/java/com/concrete/desafio/DesafioApplicationTests.java
+++ src/test/java/com/concrete/desafio/DesafioApplicationTests.java
@@ -1,4 +1,4 @@
-package com.desafio;
+package com.concrete.desafio;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DesafioconcreteApplicationTests {
+public class DesafioApplicationTests {
 
 	@Test
 	public void contextLoads() {
